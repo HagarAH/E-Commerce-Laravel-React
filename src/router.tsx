f9cb1f83f0daeddb0e5b9189a4a20645e9a37381
Hagar Hamad <hagar.akrum@gmail.com>
2023-07-05 19:40:48 +0300

    init

---
+++ src/router.tsx
@@ -0,0 +1,23 @@
+import {createBrowserRouter} from "react-router-dom";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+import NotFound from "./views/NotFound";
+
+const router =createBrowserRouter([
+    {
+        path:'/login',
+        element:<Login/>
+    },
+    {
+        path:'/signup',
+        element:<Signup/>
+    },
+    {
+        path:'*',
+        element:<NotFound/>
+    }
+
+
+])
+
+export default router;
