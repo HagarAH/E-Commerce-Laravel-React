cd1046c458efed9cdd47505e458bf7770c05e869
Hagar Hamad <hagar.akrum@gmail.com>
2023-07-07 03:09:52 +0300

    Sidebar finished

--- src/views/Sidebar/index.tsx
+++ src/views/Sidebar/index.tsx
@@ -1,47 +1,98 @@
 import './index.css';
-import { NavLink } from "react-router-dom";
-import images from '../../assets/images/Group.png';
+import { Link, NavLink } from "react-router-dom";
+import logo from '../../assets/images/Group.png';
+import bars from '../../assets/images/bars.svg';
+import { useState } from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTimes} from "@fortawesome/free-solid-svg-icons";
 
 // @ts-ignore
 export default function Sidebar({ token }) {
+    const [mobile, setMobile] = useState(false);
+
     return (
         <>
-            <div className="d-flex justify-content-between nav-bar">
-                <nav className="d-flex flex-row font-nav">
-                    <NavLink to={'/home'} className="nav-link">Home</NavLink>
-                    <NavLink to={'/home'} className="nav-link">Burgers</NavLink>
-                    <NavLink to={'/home'} className="nav-link">Contact</NavLink>
-                </nav>
+            <div className={`d-flex justify-content-between ${mobile ? 'mobile-show' : 'nav-bar'}`}>
+                {mobile ? (
+                    <div>
+                        <div className="d-flex justify-content-end mx-4 my-2"  onClick={() => setMobile(false)}>
+                            <FontAwesomeIcon icon={faTimes} size={'2xl'} color={"#486284"}/>
 
-                <div>
-                    <img  src={images} alt="Logo" />
-                </div>
-
-                {token ?
-                    <div className="d-flex flex-row font-nav">
-                        <button>
-                            Careers
-                        </button>
-                        <button>
-                            Order
-                        </button>
-                    </div>
-                    :
-                    <nav>
-                        <div className="d-flex  buttons-container">
-                            <button className="button-style">
-                                Login
-                            </button>
-                            <button className="button-style"
-                                    style={{
-                                        background:'#486284',
-                                        color:"white"}}>
-
-                                Sign Up
-                            </button>
                         </div>
-                    </nav>
-                }
+
+
+                        <nav className="d-flex flex-column align-content-center  my-5 font-nav">
+                            <NavLink to={'/home'} onClick={() => setMobile(false)} className="nav-link">Home</NavLink>
+                            <NavLink to={'/home'} onClick={() => setMobile(false)} className="nav-link">Burgers</NavLink>
+                            <NavLink to={'/home'} onClick={() => setMobile(false)} className="nav-link">Contact</NavLink>
+                            <div className="d-flex flex-column justify-content-center">
+                                {token ? (
+                                    <>
+                                        <NavLink className="button-style my-2 d-flex justify-content-center" onClick={() => setMobile(false)} to='/login'>
+                                            Careers
+                                        </NavLink>
+                                        <NavLink className="button-style my-2 d-flex justify-content-center" style={{ background: '#486284', color: "white" }} onClick={() => setMobile(false)} to='/signup'>
+                                            Order
+                                        </NavLink>
+                                    </>
+                                ) : (
+                                    <>
+                                        <NavLink className="button-style my-2 d-flex justify-content-center" onClick={() => setMobile(false)} to='/login'>
+                                            Login
+                                        </NavLink>
+                                        <NavLink className="button-style my-2 d-flex justify-content-center" style={{ background: '#486284', color: "white" }} onClick={() => setMobile(false)} to='/signup'>
+                                            Sign Up
+                                        </NavLink>
+                                    </>
+                                )}
+
+                            </div>
+                        </nav>
+
+
+                    </div>
+
+
+                ) : (
+                    <>
+                        <nav className="flex-md flex-row font-nav">
+                            <NavLink to={'/home'} className="nav-link">Home</NavLink>
+                            <NavLink to={'/home'} className="nav-link">Burgers</NavLink>
+                            <NavLink to={'/home'} className="nav-link">Contact</NavLink>
+                        </nav>
+
+                        <Link to="/">
+                            <img src={logo} alt="Logo" />
+                        </Link>
+                        <nav>
+                            <div className="flex-sm buttons-container">
+                                {token ? (
+                                    <>
+                                        <NavLink className="button-style" onClick={() => setMobile(false)} to='/login'>
+                                            Careers
+                                        </NavLink>
+                                        <NavLink className="button-style" style={{ background: '#486284', color: "white" }} onClick={() => setMobile(false)} to='/signup'>
+                                            Order
+                                        </NavLink>
+                                    </>
+                                ) : (
+                                    <>
+                                        <NavLink className="button-style" onClick={() => setMobile(false)} to='/login'>
+                                            Login
+                                        </NavLink>
+                                        <NavLink className="button-style" style={{ background: '#486284', color: "white" }} onClick={() => setMobile(false)} to='/signup'>
+                                            Sign Up
+                                        </NavLink>
+                                    </>
+                                )}
+
+                            </div>
+                        </nav>
+                        <img onClick={() => setMobile(true)} className="bars" src={bars} alt="Bars" />
+
+                    </>
+                )}
+
             </div>
         </>
     );
