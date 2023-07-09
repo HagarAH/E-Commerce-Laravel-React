f8d1b35bfed33123a5e251d9a7149f49b0b7a6a7
Hagar Hamad <hagar.akrum@gmail.com>
2023-07-09 21:11:13 +0300

    Cart added

---
+++ src/views/Cart/index.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+export default function Cart(){
+        return (
+        <>
+            <div className='cart-div border-1'>
+                <h1> My cart</h1>
+
+            </div>
+
+        </>
+)
+
+}
