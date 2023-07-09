e266137495e54d235fa53a1e9c3293c186f4cebd
Hagar Hamad <hagar.akrum@gmail.com>
2023-07-09 16:38:55 +0300

    Burgers screen added

--- src/views/Burgers/index.tsx
+++ src/views/Burgers/index.tsx
@@ -1,15 +1,25 @@
 import './index.css'
+import burger from '../../assets/images/burgerExp.png'
 
 export default function Burgers() {
     return (
         <>
             <div className=" burgers-div">
 
-                <h1>Burgers</h1>
-                <p>Welcome to Burger Bliss,
-                    where we take your cravings to a whole new level!
-                    ur mouthwatering burgers are made from 100% beef and are served on freshly baked buns.
-                </p>
+               <div className="text-div">
+                   <h1 className="h1-text">Burgers</h1>
+                   <p className="burgers-p">Welcome to Burger Bliss, where we take your cravings to a whole new level!
+                       Our mouthwatering burgers are made from 100% beef and are served on freshly baked buns.
+                   </p>
+               </div>
+
+                <div className="img-div">
+
+                    <img src={burger}/>
+                    <img src={burger}/>
+                    <img src={burger}/>
+                </div>
+
             </div>
 
 
